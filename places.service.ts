39846d812d35bd1ddfab9e098ac66a98a6d97fd1
supritaph <44474147+supritaph@users.noreply.github.com>
2019-07-25 18:46:00 +0530

    commit

--- places.service.ts
+++ places.service.ts
@@ -1,40 +1,94 @@
 import { Injectable } from '@angular/core';
-
+import{take, map} from 'rxjs/operators'
 import { Place } from './place.model';
-
-
+import { AuthService } from '../auth/auth.service';
+import {  HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places: Place[] = [
+  private _places = new BehaviorSubject<Place[]>([
     new Place(
       'p1',
       'Manhattan Mansion',
       'In the heart of New York City.',
       'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       'p2',
       "L'Amour Toujours",
       'A romantic place in Paris!',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     )
-  ];
-
+  ]);
   get places() {
-    return [...this._places];
+    return this._places.asObservable();
   }
 
-  constructor() {}
+
+  constructor(private authService: AuthService, private http:HttpClient) {}
+
+  getPlace(id: string) {
+    return this.places.pipe(take(1), map(places=> {
+      return { ...places.find(p => p.id === id)};
+    }));
+   
+  }
+
+
+  addPlace(
+    title: string,
+    description: string,
+    price: number,
+    dateFrom: Date,
+    dateTo: Date
+  ) {
+    const newPlace = new Place(
+      Math.random().toString(),
+      title,
+      description,
+      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+      price,
+      dateFrom,
+      dateTo,
+      this.authService.userId
+    );
+    this.places.pipe(take(1)).subscribe(places=>{
+      this._places.next(places.concat(newPlace));
+    });
+
+    
 }
+
+
+
+
+
+
+
+
+}
+
+
+
+
