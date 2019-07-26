02fdea2f290f3b7cd792ff3e1a8f15f6853a02f4
supritaph <44474147+supritaph@users.noreply.github.com>
2019-07-26 18:34:09 +0530

    commit

---
+++ place-detail.page.ts
@@ -0,0 +1,189 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  NavController,
+  ModalController,
+  ActionSheetController,
+  LoadingController
+} from '@ionic/angular';
+import { Subscription } from 'rxjs';
+
+import { PlacesService } from '../../places.service';
+import { Place } from '../../place.model';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
+import { BookingService } from '../../../bookings/booking.service';
+import { AuthService } from '../../../auth/auth.service';
+
+@Component({
+  selector: 'app-place-detail',
+  templateUrl: './place-detail.page.html',
+  styleUrls: ['./place-detail.page.scss']
+})
+export class PlaceDetailPage implements OnInit, OnDestroy {
+  place: Place;
+  
+  private placeSub: Subscription;
+
+  constructor(
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
+    private modalCtrl: ModalController,
+    private actionSheetCtrl: ActionSheetController,
+    private bookingService: BookingService,
+    private loadingCtrl: LoadingController,
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+      this.placeSub = this.placesService
+        .getPlace(paramMap.get('placeId'))
+        .subscribe(place => {
+          this.place = place;
+          
+        });
+    });
+  }
+
+  onBookPlace() {
+    // this.router.navigateByUrl('/places/tabs/discover');
+    // this.navCtrl.navigateBack('/places/tabs/discover');
+    // this.navCtrl.pop();
+    this.actionSheetCtrl
+      .create({
+        header: 'Choose an Action',
+        buttons: [
+          {
+            text: 'Select Date',
+            handler: () => {
+              this.openBookingModal('select');
+            }
+          },
+          {
+            text: 'Random Date',
+            handler: () => {
+              this.openBookingModal('random');
+            }
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          }
+        ]
+      })
+      .then(actionSheetEl => {
+        actionSheetEl.present();
+      });
+  }
+
+  openBookingModal(mode: 'select' | 'random') {
+    console.log(mode);
+    this.modalCtrl
+      .create({
+        component: CreateBookingComponent,
+        componentProps: { selectedPlace: this.place, selectedMode: mode }
+      })
+      .then(modalEl => {
+        modalEl.present();
+        return modalEl.onDidDismiss();
+      })
+      .then(resultData => {
+        console.log(resultData.data, resultData.role);
+        if (resultData.role === 'confirm') {
+          this.loadingCtrl
+          .create({ message: 'Booking place...' })
+          .then(loadingEl => {
+            loadingEl.present();
+            const data = resultData.data.bookingData;
+            this.bookingService
+              .addBooking(
+                this.place.id,
+                this.place.title,
+                this.place.imageUrl,
+                data.firstName,
+                data.lastName,
+                data.guestNumber,
+                data.startDate,
+                data.endDate
+              )
+              .subscribe(() => {
+                loadingEl.dismiss();
+              });
+          });
+      }
+    });
+}
+
+  
+
+  ngOnDestroy() {
+    if (this.placeSub) {
+      this.placeSub.unsubscribe();
+    }
+  }
+}
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { Router, ActivatedRoute } from '@angular/router';
+// import { NavController, ModalController } from '@ionic/angular';
+// import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
+// import { PlacesService } from '../../places.service';
+// import { Place } from '../../place.model';
+
+
+// @Component({
+//   selector: 'app-place-detail',
+//   templateUrl: './place-detail.page.html',
+//   styleUrls: ['./place-detail.page.scss'],
+// })
+// export class PlaceDetailPage implements OnInit {
+//   place: Place;
+  
+  
+
+//   constructor( 
+//     private navCtrl: NavController,
+//     private modalCtrl: ModalController,
+//     private route: ActivatedRoute,
+//     private placesService: PlacesService,) { }
+
+//   ngOnInit() {
+//     {
+//       this.route.paramMap.subscribe(paramMap => {
+//         if (!paramMap.has('placeId')) {
+//           this.navCtrl.navigateBack('/places/tabs/discover');
+//           return;
+//         }
+//   }
+
+//   onBookPlace()
+//   {
+//     // this.router.navigateByUrl('/places/tabs/discover');
+//     // this.navCtrl.navigateBack('/places/tabs/discover');
+//   // this.navCtrl.pop();
+
+//   this.modalCtrl.create({
+//     component: CreateBookingComponent,
+//     componentProps: { selectedPlace: this.place }
+//   })
+//   .then(modalEl => {
+//     modalEl.present();
+//     // return modalEl.onDidDismiss();
+//   });
+//   /*.then(resultData => {
+//     console.log(resultData.data, resultData.role);
+//     if (resultData.role === 'confirm') {
+//       console.log('BOOKED!');
+//     }
+//   });
+// */}
+// }
--- places.service.ts
+++ places.service.ts
@@ -1,40 +1,241 @@
+
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 
 import { Place } from './place.model';
-
-
+import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places: Place[] = [
+  private _places = new BehaviorSubject<Place[]>([
     new Place(
       'p1',
       'Manhattan Mansion',
       'In the heart of New York City.',
       'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'xyz'
     ),
     new Place(
       'p2',
       "L'Amour Toujours",
       'A romantic place in Paris!',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
+      'abc'
     )
-  ];
+  ]);
 
   get places() {
-    return [...this._places];
+    return this._places.asObservable();
   }
 
-  constructor() {}
+  constructor(private authService: AuthService, private http: HttpClient) {}
+
+  getPlace(id: string) {
+    return this.places.pipe(
+      take(1),
+      map(places => {
+        return { ...places.find(p => p.id === id) };
+      })
+    );
+  }
+
+  addPlace(
+    title: string,
+    description: string,
+    price: number,
+    dateFrom: Date,
+    dateTo: Date
+  ) {
+    let generatedId: string;
+    const newPlace = new Place(
+      Math.random().toString(),
+      title,
+      description,
+      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+      price,
+      dateFrom,
+      dateTo,
+      this.authService.userId
+    );
+    return this.http
+      .post<{name: string}>(
+        'https://ionic-angular-94378.firebaseio.com/offered-places.json',
+        {
+          ...newPlace,
+          id: null
+        }
+      )
+      .pipe(
+        switchMap(resData=> {
+          generatedId= resData.name;
+          return this.places;
+        }),
+        take(1),
+        tap(places=> {
+          newPlace.id=generatedId;
+          this._places.next(places.concat(newPlace));
+            })
+      
+      );
+    
+    // return this.places.pipe(
+    //   take(1),
+    //   delay(1000),
+    //   tap(places => {
+    //     this._places.next(places.concat(newPlace));
+    //   })
+    // );
+  }
+
+  updatePlace(placeId: string, title: string, description: string) {
+    return this.places.pipe(
+      take(1),
+      delay(1000),
+      tap(places => {
+        const updatedPlaceIndex = places.findIndex(pl => pl.id === placeId);
+        const updatedPlaces = [...places];
+        const oldPlace = updatedPlaces[updatedPlaceIndex];
+        updatedPlaces[updatedPlaceIndex] = new Place(
+          oldPlace.id,
+          title,
+          description,
+          oldPlace.imageUrl,
+          oldPlace.price,
+          oldPlace.availableFrom,
+          oldPlace.availableTo,
+          oldPlace.userId
+        );
+        this._places.next(updatedPlaces);
+      })
+    );
+  }
+ 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Injectable } from '@angular/core';
+// import{take, map} from 'rxjs/operators'
+// import { Place } from './place.model';
+// import { AuthService } from '../auth/auth.service';
+// import {  HttpClient } from '@angular/common/http';
+// import { BehaviorSubject } from 'rxjs';
+
+
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class PlacesService {
+//   private _places = new BehaviorSubject<Place[]>([
+//     new Place(
+//       'p1',
+//       'Manhattan Mansion',
+//       'In the heart of New York City.',
+//       'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+//       149.99,
+//       new Date('2019-01-01'),
+//       new Date('2019-12-31'),
+//       'abc'
+//     ),
+//     new Place(
+//       'p2',
+//       "L'Amour Toujours",
+//       'A romantic place in Paris!',
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+//       189.99,
+//       new Date('2019-01-01'),
+//       new Date('2019-12-31'),
+//       'abc'
+//     ),
+//     new Place(
+//       'p3',
+//       'The Foggy Palace',
+//       'Not your average city trip!',
+//       'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+//       99.99,
+//       new Date('2019-01-01'),
+//       new Date('2019-12-31'),
+//       'abc'
+//     )
+//   ]);
+//   get places() {
+//     return this._places.asObservable();
+//   }
+
+
+//   constructor(private authService: AuthService, private http:HttpClient) {}
+
+//   getPlace(id: string) {
+//     return this.places.pipe(take(1), map(places=> {
+//       return { ...places.find(p => p.id === id)};
+//     }));
+   
+//   }
+
+
+//   addPlace(
+//     title: string,
+//     description: string,
+//     price: number,
+//     dateFrom: Date,
+//     dateTo: Date
+//   ) {
+//     const newPlace = new Place(
+//       Math.random().toString(),
+//       title,
+//       description,
+//       'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+//       price,
+//       dateFrom,
+//       dateTo,
+//       this.authService.userId
+//     );
+//     this.places.pipe(take(1)).subscribe(places=>{
+//       this._places.next(places.concat(newPlace));
+//     });
+
+    
+// }
+
+
+
+
+
+// }
+
+
+
+
